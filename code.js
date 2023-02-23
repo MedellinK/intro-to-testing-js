2d79981272a8570c7bc33ef5227bc54929401564
Kelvyn Medellin <medellinkelvyn98@gmail.com>
2023-02-23 09:50:53 -0600

    add the first test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -1,4 +1,13 @@
-// helloWorld function 
-function helloWorld() {
+// helloWorld function
+// function helloWorld() {
+//     return "Hello, World!";
+//     // console.log( "Hello, World!")
+// }
+
+const helloWorld = function() {
     return "Hello, World!";
 }
+
+// const sayHello = function (){
+//     return `Hello, World!`
+// }
